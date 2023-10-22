ff8c04d673d3c7a102450eb41ad2b2bf7625bbb8
Hannes Alexandersson <135141396+HannesAlexandersson@users.noreply.github.com>
2023-10-22 17:20:25 +0200

    initial commit
    Basic file layout, and magic navbar implented. Started with object-arrays for building of item cards

---
+++ footer.php
@@ -0,0 +1,10 @@
+</body>
+        <footer>
+            <div class="footer-container">            
+                <h3>Social Media:</h3>
+                <p>Insta: </p>
+                <p>Meta:</p>                           
+            </div>        
+        </footer>
+    </body>
+</html>
---
+++ object-array.php
@@ -0,0 +1,147 @@
+<?php
+// array containing the different snacks
+$objectArray = [
+    'Nuts' =>
+    [
+        'Roasted-peanuts' =>
+        [
+            'price' => 30,
+            'allergic' => 'peanut',
+            'bild' => 'assets/peanut.jpg'        
+        ],
+        'Roasted-almonds' =>
+        [
+            'price' => 30,
+            'allergic' => 'peanut',
+            'bild' => 'assets/peanut.jpg'        
+        ],
+        'Pistachios' =>
+        [
+            'price' => 30,
+            'allergic' => 'peanut',
+            'bild' => 'assets/peanut.jpg'        
+        ],
+        'Cashews' =>
+        [
+            'price' => 30,
+            'allergic' => 'peanut',
+            'bild' => 'assets/peanut.jpg'        
+        ]
+    ],
+    'Crisps' => 
+    [   
+        'Ranch' =>
+        [
+            'pris' => 100,
+            'matrial' => 'ylle',
+            'bild' => 'assets/Ranch-crips.jpg'
+        ],
+        'Vinegar' =>
+        [
+            'pris' => 100,
+            'matrial' => 'ylle',
+            'bild' => 'assets/Vinegar-crisps.jpg'
+        ],
+        'Sourcream' =>
+        [
+            'pris' => 100,
+            'matrial' => 'ylle',
+            'bild' => 'assets/Sourcreame-crisps.jpg'
+        ],
+        'Hot' =>
+        [
+            'pris' => 100,
+            'matrial' => 'ylle',
+            'bild' => 'assets/Hot-crisps.jpg'
+        ]
+    ],
+    'Dip-sauce' =>
+    [   
+        'Garlic' =>
+        [
+            'pris' => 100,
+            'matrial' => 'ylle',
+            'bild' => 'assets/garlic-sauce.jpg'
+        ],
+        'bea' =>
+        [
+            'pris' => 100,
+            'matrial' => 'ylle',
+            'bild' => 'assets/bea-sauce.jpg'
+        ],
+        'curry-vinegar' =>
+        [
+            'pris' => 100,
+            'matrial' => 'ylle',
+            'bild' => 'assets/curryVinegar-sauce.jpg'
+        ],
+        'Hot' =>
+        [
+            'pris' => 100,
+            'matrial' => 'ylle',
+            'bild' => 'assets/Hot-sauce.jpg'
+        ]
+    ],
+    'Crackers' =>
+    [
+        'Cheese-crackers' =>
+        [
+            'pris' => 100,
+            'matrial' => 'ylle',
+            'bild' => 'assets/cheese-crackers.jpg'
+        ],
+        'PeanutButter-crackers' =>
+        [
+            'pris' => 100,
+            'matrial' => 'ylle',
+            'bild' => 'assets/peanutButter-crackers.jpg'
+        ]
+    ]
+];
+
+// beverage array containing the diffrent beverages
+$beverageArray = 
+[
+    'sparkling' =>
+    [
+        'Coco-cool' =>
+        [
+            'pris' => 100,
+            'matrial' => 'ylle',
+            'bild' => 'assets/cheese-crackers.jpg'
+        ],
+        'Fanto' =>
+        [
+            'pris' => 100,
+            'matrial' => 'ylle',
+            'bild' => 'assets/cheese-crackers.jpg'
+        ],
+        'Pepso' =>
+        [
+            'pris' => 100,
+            'matrial' => 'ylle',
+            'bild' => 'assets/cheese-crackers.jpg'
+        ],
+    ],
+    'still' =>
+    [
+        'Orango' =>
+        [
+            'pris' => 100,
+            'matrial' => 'ylle',
+            'bild' => 'assets/cheese-crackers.jpg'
+        ],
+        'Appleoppo' =>
+        [
+            'pris' => 100,
+            'matrial' => 'ylle',
+            'bild' => 'assets/cheese-crackers.jpg'
+        ],
+        'Peach' =>
+        [
+            'pris' => 100,
+            'matrial' => 'ylle',
+            'bild' => 'assets/cheese-crackers.jpg'
+        ]
+    ]
+];
